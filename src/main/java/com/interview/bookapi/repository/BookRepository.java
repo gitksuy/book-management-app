901adcf2f58064bda23b4065f67e35be170b2cb7
ahmad.yazid <yazidharharah@hotmail.com>
2025-04-27 18:14:18 +0700

    Add repository layer for data access

---
+++ src/main/java/com/interview/bookapi/repository/BookRepository.java
@@ -0,0 +1,17 @@
+package com.interview.bookapi.repository;
+
+import com.interview.bookapi.entity.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+    Page<Book> findByTitleContainingIgnoreCase(String title, Pageable pageable);
+    Page<Book> findByAuthorContainingIgnoreCase(String author, Pageable pageable);
+    Page<Book> findByCategoryContainingIgnoreCase(String category, Pageable pageable);
+    Optional<Book> findByIsbn(String isbn);
+}
